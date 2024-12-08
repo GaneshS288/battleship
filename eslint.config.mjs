5dccc00d6f0701bc71f276764b1432f3d019444e
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-12-08 16:35:30 +0530

    add type module to package.json and configure eslint to work with jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,15 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {languageOptions: { globals: globals.browser }},
+  {
+    "overrides": [
+      {
+        "files": ["tests/**/*"],
+        "env": {
+          "jest": true
+        }
+      }
+    ]
+  },
   pluginJs.configs.recommended,
 ];
